eb4c943dbca6ebbe13ab139d2b109391d08230f6
unknown <bendikss@itk-d669.win.ntnu.no>
2017-09-08 15:40:17 +0200

    Dag 2.5 (det er freedag så hvaskjeeera)

--- test/test/XMEM_module.c
+++ test/test/XMEM_module.c
@@ -5,12 +5,16 @@
  *  Author: bendikss
  */ 
 #include "XMEM_module.h"
+#include "macaroni.h"
 #include <avr/io.h>
 //Flytt denne inn i SRAM istedet?
 
 void XMEM_init(void){
 	
-	//DDRE |= (1 << PE1); Trenger vi denne? spør?
-	MCUCR = (1 << SRE);
+	//DDRE |= (1 << PE1); //Trenger vi denne? spør?
+	//MCUCR = (1 << SRE);
+	set_bit(MCUCR, SRE);
+	//SFIOR = (1 << XMM2);
+	set_bit(MCUCR, SRE);
 }
 
