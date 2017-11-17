9ed48b9a306e931e8905481327f813ee33f86dd4
Bendik Standal <bendikss@stud.ntnu.no>
2017-11-17 15:29:06 +0100

    Update PINGPONG.c

--- Nodes/Node2/PINGPONG.c
+++ Nodes/Node2/PINGPONG.c
@@ -24,32 +24,33 @@ void PINGPONG_init(){
 	
 	sei();
 	MOTOR_init();
+	CAN_transmit_game(false);
+	_delay_us(10); //TEST
 	PID_init();
 		
 }
 
 void PINGPONG_play(){
-	CAN_transmit_game(false);
+	
 	_delay_us(50);
 	uint8_t counter = 0;
 	int testvar = 0;
-	bool lose = false; //TESTVERDI, TA VEKK??	
+	bool lose = false;	
 	while (!lose)
 	{
 		counter++;
 		testvar += IR_read();
 		_delay_us(50);
-		if (counter == 5){   //digital low pass filter
-			if(testvar/5 < 9 && !lose){
-		
-			lose = true;
-
-			
-		}
+		if (counter == 5) //digital low pass filter
+		{   
+			if(testvar/5 < 9 && !lose)
+			{
+				lose = true;
+			}
 		testvar = 0;
 		counter = 0;
 		}
-		printf("tranceived from CAN: %d\n", CAN_get_curr().data[2]);
+		//printf("tranceived from CAN: %d\n", CAN_get_curr().data[2]);
 		PWM_joystick_control(CAN_get_curr().data[0]);
 				if (CAN_get_curr().data[3]) {
 			SOLENOID_shoot();
