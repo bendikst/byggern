f7372928d6cd9e97443f9c0b3a03e32e09bd94ea
Bendik Standal <bendikss@stud.ntnu.no>
2017-11-17 15:26:34 +0100

    Update PINGPONG_game.h

--- Nodes/Node2/PINGPONG_game.h
+++ Nodes/Node2/PINGPONG_game.h
@@ -1,8 +1,10 @@
 /*
  * PINGPONG_game.h
- *
+ * 
+ * Module for playing the pingpong game in Node 2 made in TTK4155 - Embedded and Industrial computer systems design.
+ * 
  * Created: 13.11.2017 16:17:06
- *  Author: bendikss
+ *  Author: Aleksander Asp, Alexander Johansen, Bendik Standal
  */ 
 
 
@@ -12,11 +14,10 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-
+/*This initializes the hardware and software for playing the pingpong game. In its current state, the initial position 
+of the motor will be decided  by where it was when the last game was lost.*/
 void PINGPONG_init(void);
 void PINGPONG_play(void);
-bool PINGPONG_lose(void);
-
 
 
 #endif /* PINGPONG_GAME_H_ */
